31a6d5f6a00a46fc4a77f156428eb87ae98dbff3
Mauro Flores <55516797+Floree1@users.noreply.github.com>
2025-12-18 20:53:43 -0300

    Add files via upload

--- auth/auth.py
+++ auth/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, session, current_app
+import os
 from flask_login import login_user, logout_user, login_required, current_user
 from inventory_system.db.models import User, db, Log
 from werkzeug.security import check_password_hash
